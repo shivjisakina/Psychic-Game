f8447657d0bae4f2a686d13b88d1fcaab36adec6
shivjisakina <shivjisakina@gmail.com>
2017-05-23 14:03:50 -0400

    added html and started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//variables
+
+var // this wont work because it has to be random letter = ("l", "s", "f", "t"),
+    wins,
+    loses,
+    guessesleft,
+    guessesfar,
+    wins,
+    // options = ("a", "b, "c", "d","e", "f", "g", "h", "i" "j", "k", "l, "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"),
+
+
+
+
+//if else statments
+
+    if (guessesfar == letter) {
+
+}
