559a7beb80c610ffb2c183f023526a7e17e94a63
shivjisakina <shivjisakina@gmail.com>
2017-05-23 15:25:01 -0400

    added logic but it only works with a prompt for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,35 @@
 //variables
 
-var // this wont work because it has to be random letter = ("l", "s", "f", "t"),
-    wins,
-    loses,
-    guessesleft,
-    guessesfar,
-    wins,
-    // options = ("a", "b, "c", "d","e", "f", "g", "h", "i" "j", "k", "l, "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"),
+var alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// this wont work because it has to be random letter = ("l", "s", "f", "t"),
 
 
 
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerGuess);
+    console.log("random letter " + computerGuess)
 
-//if else statments
+var wins = 0,
+    losses = 0,
+    guessesLeft = 10;
 
-    if (guessesfar == letter) {
-
+var userGuess = prompt("Enter your guess here "); {
+    console.log("your choice was " + userGuess)
 }
+
+
+
+document.onkeyup = function (event) {
+    
+};
+
+
+
+//if else statements
+
+    if (userGuess === alphabet) {
+        wins++
+}
+
+    
+
