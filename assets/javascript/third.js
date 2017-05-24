685afe5da5450a454c5ce540f1fbc22d8d91d75f
shivjisakina <shivjisakina@gmail.com>
2017-05-24 07:15:45 -0400

    fixed the javascript logic and console logging

--- assets/javascript/third.js
+++ assets/javascript/third.js
@@ -1,60 +1,71 @@
 
 // Declaring all of the variables in the array
-var alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
 // Declaring the wins, losses, and guessesLeft variables
-wins = 0,
-losses = 0,
-guessesLeft = 10;
+    wins = 0,
+    losses = 0;
+var guessesLeft = 10;
+
+
+// This makes the computer generate a random alphabet and logs it in the console
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 // When the user presses a key, its runs the function(game)
 document.onkeyup = function(game) {
 
     // The key that has been pressed will show up here
     var userGuess = game.key;
-
-    // This makes the computer generate a random alphabet and logs it in the console
-    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log("user guess", userGuess, "computer alphabet", computerGuess);
+
 
     // If the users guess is the sae value and type as the computer generated letter, the wins increase by 1
     if (userGuess === computerGuess) {
         wins++;
+        initateGuess();
     }
 
     // if the users guess doesnt = the computers guess and they've used all of their guesses, they get a loss
-    else if (userGuess != computerGuess) {
-        if (guessesLeft => 0) {
+    else if (userGuess !== computerGuess) {
+        if (guessesLeft !== 0) {
             guessesLeft--;
-        }
+            console.log("guesses left ", guessesLeft);
 
+
+        }
         // Losses increase if all 10 guesses are wrong
-        if (guessesLeft === 0) {
+        else if (guessesLeft === 0) {
             losses++;
+            console.log("guessesleft equal to 0", guessesLeft);
+            initateGuess();
         }
 
+    }
+    /* this changed the losses everytime instead of every 10 times
+     else (userGuess !== computerGuess); {
 
-        /* this changed the losses everytime instead of every 10 times
-        else (userGuess !== computerGuess); {
+     losses++;
 
-             losses++;
-
-        }
-         */
-
-
-
-        // This combines the html and js variables that will show up on the page
-        var html = "<h1>The Psychic Game</h1>" +
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + losses + "</p>" +
-            "<p>Guesses left: " + guessesLeft + "</p>" +
-            "<p>Your guesses so far: " + userGuess + "</p>";
-
-        // Using innerHTML so declare the variable values in the game.
-        document.querySelector("#game").innerHTML = html;
-
-    };
-};
+     }
+     */
 
 
+    // This combines the html and js variables that will show up on the page
+    var html = "<h1>The Psychic Game</h1>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft + "</p>" +
+        "<p>Your guesses so far: " + userGuess + "</p>";
+
+    // Using innerHTML so declare the variable values in the game.
+    document.querySelector("#game").innerHTML = html;
+
+
+}
+
+function initateGuess(){
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    guessesLeft = 10;
+    console.log("New guess begins", computerGuess);
+
+}
